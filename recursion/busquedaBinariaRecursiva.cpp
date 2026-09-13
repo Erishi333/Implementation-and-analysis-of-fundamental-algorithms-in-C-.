fb83d441a667a22daa4a17611f2a98276751e244
Erick <88639846+Erishi333@users.noreply.github.com>
2026-09-13 18:40:57 -0500

    Crear recursion/busquedaBinariaRecursiva.cpp

---
+++ recursion/busquedaBinariaRecursiva.cpp
@@ -0,0 +1,26 @@
+int busquedaBinariaRecursiva(
+    const vector<int>& arreglo,
+    int objetivo,
+    int izquierda,
+    int derecha
+) {
+    if (izquierda > derecha) {
+        return -1;
+    }
+
+    int medio = izquierda + (derecha - izquierda) / 2;
+
+    if (arreglo[medio] == objetivo) {
+        return medio;
+    }
+
+    if (arreglo[medio] < objetivo) {
+        return busquedaBinariaRecursiva(
+            arreglo, objetivo, medio + 1, derecha
+        );
+    }
+
+    return busquedaBinariaRecursiva(
+        arreglo, objetivo, izquierda, medio - 1
+    );
+}
